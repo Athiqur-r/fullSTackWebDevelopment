6ae2251d19bac9a41d4debb3e37b2de0f5831173
InnomickPradeep <pradeep.t@innomick.com>
2021-05-26 23:11:16 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/',function(req,res){
+    res.send('<h1>Hello World</h1>');
+});
+
+app.listen(7000,function(){
+    console.log("Server listening to port 7000");
+});
